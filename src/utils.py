9a96655c2f97368845cd1988988bda9cbad6a02f
Mario Vergara <mario.a.vergara.l@gmail.com>
2020-05-05 17:04:34 -0400

    fixed decorator

--- src/utils.py
+++ src/utils.py
@@ -8,7 +8,6 @@ def execute_or_load_csv(func):
     def wrapper(*args, path=None, overwrite=True, **kwargs):
         if path is None:
             res = func(*args, **kwargs)
-            res.to_csv(path, index=False)
             return res
         if not os.path.exists(path) or overwrite:
             res = func(*args, **kwargs)
